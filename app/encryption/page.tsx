fab9c8215da8f43e2b6f446a4824d423f041e259
Henry <contact@henrywa.ng>
2024-10-11 11:50:00 -0700

    some css

--- app/encryption/page.tsx
+++ app/encryption/page.tsx
@@ -6,6 +6,7 @@ import TitleInput from '../../components/TitleInput';
 import ContentArea from '../../components/ContentArea';
 import ScrollContainer from '../../components/ScrollContainer';
 import styles from '../../styles/page.module.css';
+import encryptStyles from '../../styles/EncryptButton.module.css';
 import { pageTitle, pageContent } from './pageContent'; 
 
 export default function Paste() {
@@ -14,20 +15,31 @@ export default function Paste() {
   const title = pageTitle;
   const content = pageContent;
 
+
   return (
     <div className={styles.pageContainer}>
-      <PageHeader/>
+      <PageHeader />
       <ScrollContainer>
         <div className={styles.contentWrapper}>
-          <TitleInput
-            initialTitle={title}
-            titleEditableRef={titleEditableRef}
-            isEditable={false} 
-          />
-          <ContentArea
-            content={content}
-            isEditable={false} 
-          />
+          <span style={{ display: 'inline-flex', alignItems: 'center' }}>
+            <TitleInput
+              initialTitle={title}
+              titleEditableRef={titleEditableRef}
+              isEditable={false}
+            />
+            
+            <label
+              className={`${encryptStyles.btnLock} ${encryptStyles.locked}`}
+              style={{ cursor: 'default', marginLeft: '0.3em'}}
+            >
+              <svg width="20" height="20" viewBox="0 0 36 40" style={{ verticalAlign: 'middle', marginTop: '-5px' }}>
+                <path className="lockb" d="M27 27C27 34.1797 21.1797 40 14 40C6.8203 40 1 34.1797 1 27C1 19.8203 6.8203 14 14 14C21.1797 14 27 19.8203 27 27ZM15.6298 26.5191C16.4544 25.9845 17 25.056 17 24C17 22.3431 15.6569 21 14 21C12.3431 21 11 22.3431 11 24C11 25.056 11.5456 25.9845 12.3702 26.5191L11 32H17L15.6298 26.5191Z"></path>
+                <path className="lock" d="M6 21V10C6 5.58172 9.58172 2 14 2V2C18.4183 2 22 5.58172 22 10V21"></path>
+              </svg>
+            </label>
+          </span>
+
+          <ContentArea content={content} isEditable={false} />
         </div>
       </ScrollContainer>
     </div>
