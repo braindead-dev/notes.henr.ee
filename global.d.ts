2aa572f90a591837fc2c3e3a5250aaf43c64845d
Henry <contact@henrywa.ng>
2024-11-05 02:39:06 -0800

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,7 @@
 import { MongoClient } from 'mongodb';
 
-// This is required to make TypeScript treat this file as a module.
-export {};
-
 declare global {
-  // Add the _mongoClientPromise to the global namespace
-  var _mongoClientPromise: Promise<MongoClient> | null;
-
-  // Retain the pastes variable you already have
-  var pastes: Map<string, { title: string; content: string }>;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
+
+export {};
